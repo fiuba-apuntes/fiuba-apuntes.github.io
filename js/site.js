7e6aace1d55ce54453c180ea8fbc12ebfe1c0174
Ezequiel Pérez Dittler <ezeperez26@users.noreply.github.com>
2015-02-22 13:38:40 -0300

    Traslado el codigo js del sitio a un archivo separado (site.js) y permito la apertura multiple en el acordeón.

---
+++ js/site.js
@@ -0,0 +1,9 @@
+$(document).foundation({
+    accordion: {
+        // allow multiple accordion panels to be active at the same time
+        multi_expand: true,
+        // allow accordion panels to be closed by clicking on their headers
+        // setting to false only closes accordion panels when another is opened
+        toggleable: true
+    }
+});
